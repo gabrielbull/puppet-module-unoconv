7f0bbf9482bdcc674cebe5292333b62c6b40ce7c
Gabriel Bull <me@gabrielbull.com>
2014-02-16 17:23:47 -0500

    Added required dependancies

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,21 @@
 class unoconv () {
+  package {"libreoffice":
+    ensure => "present",
+  }
+  package {"libreoffice-headless":
+    ensure => "present",
+  }
+  package {"libreoffice-sdk":
+    ensure => "present",
+  }
   exec { "download_unoconv":
     path   => "/usr/bin:/usr/sbin:/bin",
     command => "curl https://codeload.github.com/dagwieers/unoconv/tar.gz/0.6 | tar -xz",
-    require => Package['libreoffice'],
+    require => [
+      Package['libreoffice'],
+      Package['libreoffice-headless'],
+      Package['libreoffice-sdk'],
+    ],
     notify => Exec["install_unoconv"],
   }
   exec { "install_unoconv":
